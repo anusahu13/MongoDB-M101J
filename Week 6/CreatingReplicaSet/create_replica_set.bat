390ee16b08c53dc9804b2c88af3644b3e0617b07
Anu Sahu <anusahu13@gmail.com>
2016-04-21 16:11:03 -0500

    Week 6
    Applicaion Engineering

---
+++ Week 6/CreatingReplicaSet/create_replica_set.bat
@@ -0,0 +1,4 @@
+
+start /b mongod --replSet m101 --logpath "1.log" --dbpath "c:\data\rs1" --port 27017 --smallfiles
+start /b mongod --replSet m101 --logpath "2.log" --dbpath "c:\data\rs2" --port 27018 --smallfiles
+start /b mongod --replSet m101 --logpath "3.log" --dbpath "c:\data\rs3" --port 27019 --smallfiles
